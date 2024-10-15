75f38c82d93ebfbf63019ae7f191ed1d04b6f093
AbrahamG99 <abrahamgomez932@gmail.com>
2024-10-15 13:21:09 -0600

    Agrego los respectivos archivos a mi repositorio en github

---
+++ tipos-de-dato.js
@@ -0,0 +1,13 @@
+console.log (typeof "texto")
+console.log (typeof 11071999)
+console.log (typeof true)
+console.log (typeof 'once de julio')
+//este es un ejemplo de comentario
+console.log (typeof null)
+console.log (typeof 0)
+console.log (typeof FALSE)
+console.log (typeof -777)
+console.log (typeof undefined)
+console.log (typeof 11/07/1999)
+/*este es un ejemplo de comentario 
+con muchas lineas */
